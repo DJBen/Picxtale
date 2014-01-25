a0c2281f9a88621e2ab23d25c142c50f2c4f49bc
DJBen <lsh32768@gmail.com>
2014-01-25 16:29:44 -0600

    Initial Commit

---
+++ PicxTale/PTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PTAppDelegate.h
+//  PicxTale
+//
+//  Created by Sihao Lu on 1/25/14.
+//  Copyright (c) 2014 Hack Rice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PicxTale/PTViewController.h
@@ -0,0 +1,13 @@
+//
+//  PTViewController.h
+//  PicxTale
+//
+//  Created by Sihao Lu on 1/25/14.
+//  Copyright (c) 2014 Hack Rice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTViewController : UIViewController
+
+@end
